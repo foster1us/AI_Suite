2ee4752b5e647c5ce60f306a9d11f12a8515fe47
foster1us <fosterius@gmail.com>
2025-11-15 21:11:42 -0500

    Add files via upload

---
+++ build/install_invokeai.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+
+# Create and activate venv
+mkdir /InvokeAI
+cd /InvokeAI
+mkdir -p /venvs
+python3 -m venv --system-site-packages /venvs/invokeai
+source /venvs/invokeai/bin/activate
+
+# Upgrade pip inside this venv to avoid old resolver bugs
+pip3 install --upgrade "pip>=24.0"   # <-- add this line
+
+# Install torch and xformers
+#pip3 install --no-cache-dir torch==${INVOKEAI_TORCH_VERSION} torchvision torchaudio --index-url ${INDEX_URL}
+#pip3 install --no-cache-dir xformers==${INVOKEAI_XFORMERS_VERSION} --index-url ${INDEX_URL}
+
+# Install InvokeAI
+pip3 install InvokeAI[xformers]==${INVOKEAI_VERSION} --use-pep517
+pip3 cache purge
+deactivate
